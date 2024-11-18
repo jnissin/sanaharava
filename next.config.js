8c65443cf48fcd11b2220e6ee2a26b270fe41cd2
Joonas Nissinen <joonas.nissinen@futurice.com>
2024-11-18 09:31:39 +0000

    Attempt to fix the dictionary file read problem in deployed instances

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    swcMinify: true,
+  reactStrictMode: true,
+  swcMinify: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      '/api/**/*': [
+        'data/**/*'
+      ]
+    }
   }
-  
-  module.exports = nextConfig
+}
+
+module.exports = nextConfig
